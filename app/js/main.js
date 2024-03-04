bcaca506c69268e9da6d760961adf6ba5d72903e
RedMoon <tv12vv@gmail.com>
2024-03-04 04:17:46 +0100

    try to make changing color svg

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,15 @@
 $(function(){
+  $('.categories__menu-item').each(function() {
+    const $svg = $(this).find('svg');
+    const $path = $svg.find('g > path');
+    const originalColor = $path.attr('fill');
+
+    $('.categories__menu-link').on('click', function() {
+      if ($('.categories__menu-link').hasClass('mixitup-control-active')) {
+        $path.attr('fill', '#ffffff');
+      } 
+    });
+  });
 
   const mixer = mixitup('.categories__dishes');
 
